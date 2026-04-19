ab863dd8b418bf10e7bc0065cd302ed48bf7f58c
codesam-com <samzortix16@gmail.com>
2026-04-19 15:05:56 +0200

    feat: add priority queue processing

--- worker/worker.py
+++ worker/worker.py
@@ -2,5 +2,5 @@ from rq import Worker
 from services.queue import redis_conn
 
 if __name__ == "__main__":
-    worker = Worker(["image-generation"], connection=redis_conn)
+    worker = Worker(["premium", "free"], connection=redis_conn)
     worker.work()
